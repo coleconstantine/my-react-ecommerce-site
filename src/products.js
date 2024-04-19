007b05d7e466799e9384698783278f9f437b564d
coleconstantine <124316356+coleconstantine@users.noreply.github.com>
2024-04-19 15:22:00 -0400

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,24 @@
+import product1 from "./assets/1iphone.png";
+import product2 from "./assets/2camera.png";
+import product3 from "./assets/3laptop.png";
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Iphone",
+        price: 999.00,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Camera",
+        price: 800.00,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Laptop",
+        price: 1200.00,
+        productImage: product3,
+    },
+];
